1e38ce29cdd8423e74c25cc39d98e892e372466a
Your Name <jacklinuxnd@gmail.com>
2024-07-21 22:51:59 +0545

    day2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { Route,Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
+// import Login from './pages/login/Login';
+import Header from './components/header/Header';
 function App() {
-  
   return (
     <div className="App">
       <Routes>
-<Route path='/' element={<Login/>}/>
+<Route path='/' element={<Header/>}/>
 
 
       </Routes>
